11f07f17b32853849fe46418ced75087e3c9e90f
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2020-03-18 13:21:01 +0100

    PR

--- bundlex.exs
+++ bundlex.exs
@@ -14,33 +14,38 @@ defmodule Membrane.LibdtlssrtpWrapper.BundlexProject do
           "dsink_udp.c",
           "dtls_srtp.c"
         ],
-        includes: ["/usr/local/opt/openssl/include"],
-        lib_dirs: ["/usr/local/opt/openssl/lib"],
         libs: ["crypto", "ssl"],
         src_base: "libdtlssrtp"
-      ],
+      ] ++ platform_dependent_options(Bundlex.platform()),
       handshaker_utils: [
         sources: [
           "dsink_udp.c",
           "dtls_srtp.c",
           "handshaker_utils.c"
         ],
-        includes: ["/usr/local/opt/openssl/include"],
-        lib_dirs: ["/usr/local/opt/openssl/lib"],
         libs: ["crypto", "ssl"],
         src_base: "libdtlssrtp"
-      ],
+      ] ++ platform_dependent_options(Bundlex.platform()),
       dummy_client: [
         sources: [
           "dummy_client.c",
           "dsink_udp.c",
           "dtls_srtp.c"
         ],
-        includes: ["/usr/local/opt/openssl/include"],
-        lib_dirs: ["/usr/local/opt/openssl/lib"],
         libs: ["crypto", "ssl"],
         src_base: "libdtlssrtp"
-      ]
+      ] ++ platform_dependent_options(Bundlex.platform())
     ]
+  end
+
+  defp platform_dependent_options(:macosx) do
+    [
+      includes: ["/usr/local/opt/openssl/include"],
+      lib_dirs: ["/usr/local/opt/openssl/lib"]
+    ]
+  end
+
+  defp platform_dependent_options(_other) do
+    []
   end
 end
